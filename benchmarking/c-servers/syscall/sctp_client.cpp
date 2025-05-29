71fef9d1e02437902686ff64ec5a7acad7d50201
GabroveanuRazvan <gabroveanu@gmail.com>
2025-05-29 10:22:13 +0300

    Changed syscalls in C++ sctp scripts.

---
+++ benchmarking/c-servers/syscall/sctp_client.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+#include <cerrno>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <chrono>
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <netinet/sctp.h>
+
+const size_t KILOBYTE = 1024;
+
+const char* REQUESTS_FILE_PATH = "../requests.txt";
+const char* PEER_IPV4 = "127.0.0.1";
+const u_int16_t PORT = 7878;
+const size_t RECEIVE_BUFFER_SIZE = 1024 * KILOBYTE;
+
+std::vector<std::string> load_requests(const std::string& filename) {
+    std::vector<std::string> requests;
+    std::ifstream infile(filename);
+    std::string request;
+
+    if (!infile) {
+        std::cerr << "Could not open file: " << filename << std::endl;
+        return requests;
+    }
+
+    while (std::getline(infile, request)) {
+        requests.push_back(request);
+    }
+
+    return requests;
+}
+
+
+std::string HttpGetHeader(const std::string& file_path) {
+
+    if (file_path.empty()) {
+        throw std::runtime_error("Empty Http Header request");
+    }
+
+    return "GET " + file_path + " HTTP/1.1\r\nHost: cpp";
+}
+
+
+unsigned long long extract_content_length(const char* buffer) {
+    const char* header = "Content-Length: ";
+    const char* found = std::strstr(buffer, header);
+
+    if (!found) return -1;
+
+    found += std::strlen(header);
+
+    return std::strtoull(found, nullptr, 10);;
+}
+
+
+int main(){
+
+    char buffer[RECEIVE_BUFFER_SIZE];
+    std::vector<std::string> requests = load_requests(REQUESTS_FILE_PATH);
+    size_t request_count = requests.size();
+
+    double total_run_time = 0;
+    size_t total_received_size = 0;
+
+    // Create the sctp socket
+    int sock_fd = socket(AF_INET,SOCK_STREAM,IPPROTO_SCTP);
+    if(sock_fd < 0){
+        std::cerr << "socket: " << std::strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Build the peer socket address
+    sockaddr_in peer_addr = {0};
+    peer_addr.sin_family = AF_INET;
+    peer_addr.sin_port = htons(PORT);
+    if(inet_pton(AF_INET, PEER_IPV4, &peer_addr.sin_addr) < 0){
+        std::cerr << "inet_pton: "<< std::strerror(errno) << std::endl;
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Reuse the address when needed
+    int option = 1;
+    if(setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option)) < 0){
+        std::cerr << "setsockopt: " << std::strerror(errno) << std::endl;
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Enable data io
+    struct sctp_event_subscribe events;
+    bzero(&events,sizeof(events));
+    events.sctp_data_io_event = 1;
+
+    if(setsockopt(sock_fd,IPPROTO_SCTP,SCTP_EVENTS,&events,sizeof(events)) < 0){
+        perror("setsockopt");
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to the peer
+    if(connect(sock_fd,(struct sockaddr*)&peer_addr,sizeof(peer_addr)) == -1){
+        std::cerr << "connect: " << std::strerror(errno) << std::endl;
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    int request_index = 0;
+
+    struct sctp_sndrcvinfo info = {0};
+    socklen_t info_len = sizeof(info);
+
+    for(const auto& request : requests){
+
+        auto header = HttpGetHeader(request);
+
+
+        // Send the request
+        if(write(sock_fd,header.c_str(),header.size()) < 0){
+            std::cerr << "Send msg: " << std::strerror(errno) << std::endl;
+            close(sock_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Receive the response
+        int bytes_received = 0;
+
+        if(( bytes_received = read(sock_fd,buffer,sizeof(buffer))) < 0){
+            std::cerr << "Recv msg: " << std::strerror(errno) << std::endl;
+            close(sock_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Connection closed
+        if(bytes_received == 0){
+            break;
+        }
+
+        // Start the timer when receiving the file
+        auto start_time = std::chrono::high_resolution_clock::now();
+
+        size_t content_length = extract_content_length(buffer);
+        size_t current_length = 0;
+
+        // Receive the data in a loop until the file is downloaded
+        while(current_length < content_length){
+
+            if(( bytes_received = read(sock_fd,buffer,sizeof(buffer))) < 0){
+                std::cerr << "Recv msg: " << std::strerror(errno) << std::endl;
+                close(sock_fd);
+                exit(EXIT_FAILURE);
+            }
+
+            // Connection closed
+            if(bytes_received == 0){
+                break;
+            }
+
+            current_length += bytes_received;
+
+        }
+
+        // Stop the timer and compute the data
+        auto end_time = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end_time - start_time;
+
+        total_run_time += duration.count();
+        total_received_size += content_length;
+
+
+        std::cout<<++request_index<<std::endl;
+    }
+
+    double throughput = (double)total_received_size / total_run_time;
+
+    std::cout<<"Total test time: "<<total_run_time<<" secs"<<std::endl;
+    std::cout<<"Average throughput: "<<throughput<<" bytes/sec"<<std::endl;
+
+    close(sock_fd);
+    return 0;
+}
---
+++ benchmarking/c-servers/syscall/sctp_http_server.cpp
@@ -0,0 +1,257 @@
+#include <iostream>
+#include <cerrno>
+#include <cstring>
+#include <string>
+#include <sstream>
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <netinet/sctp.h>
+
+const uint16_t PORT = 7878;
+const int KILOBYTE = 1024;
+const char* SERVER_ROOT = "../../benchmark_raw_dataset";
+const size_t CHUNK_SIZE = 16 * KILOBYTE;
+const size_t SENDER_BUFFER_SIZE = 1024 * KILOBYTE;
+
+uint16_t MAX_STREAM_NUM = 4;
+
+
+std::string recv_response_header(int client_fd) {
+
+    std::string buffer;
+    char header[8 * KILOBYTE];
+
+    struct sctp_sndrcvinfo recv_info = {0};
+    int bytes_received = read(client_fd, header, sizeof(header));
+
+    // Return an empty buffer if the connection closed or an error occurred
+    if(bytes_received == 0)
+        return std::move(buffer);
+
+    if (bytes_received < 0) {
+        std::cerr << "Recv: " << std::strerror(errno) << std::endl;
+        return std::move(buffer);
+    }
+
+    buffer.append(header, bytes_received);
+
+    return std::move(buffer);
+
+}
+
+// Extract the http path from a request and prepare it
+std::string extract_http_path(const std::string& request) {
+    std::istringstream request_stream(request);
+    std::string method, path, version;
+
+    request_stream >> method >> path >> version;
+
+    if(method != "GET")
+        std::cerr<<"Other verbs not supported"<<std::endl;
+
+    return path == "/" ? "./index.html" : path.insert(0,".");
+
+}
+
+// Simple http header
+std::string make_http_response_header(size_t size) {
+    std::ostringstream response;
+
+    response << "HTTP/1.1 200 OK\r\n"
+             << "Content-Length: " << size << "\r\n"
+             << "Content-Type: text/html\r\n"
+             << "Connection: Keep-Alive\r\n"
+             << "\r\n";
+
+    return response.str();
+}
+
+bool send_file(int client_fd, const std::string& file_path){
+
+    // Open the file
+    int fd = open(file_path.c_str(), O_RDONLY);
+    if (fd < 0){
+        std::cerr << "Open: " << std::strerror(errno) << std::endl;
+        close(client_fd);
+        close(fd);
+        return false;
+    }
+
+    // Get the file size
+    struct stat file_status;
+    if(fstat(fd,&file_status) < 0){
+        std::cerr << "Fstat: " << std::strerror(errno) << std::endl;
+        close(client_fd);
+        close(fd);
+        return false;
+    }
+
+    size_t file_size = file_status.st_size;
+
+    // Map the file into memory
+    char* mmap_file = (char *)mmap(nullptr,file_size,PROT_READ,MAP_PRIVATE,fd,0);
+    if(mmap_file == nullptr){
+        std::cerr << "Mmap: " << std::strerror(errno) << std::endl;
+        close(client_fd);
+        close(fd);
+        return false;
+    }
+
+    uint16_t stream_index = 0;
+
+    // Prepare a response header and send it
+    auto response_header = make_http_response_header(file_size);
+
+    if(write(client_fd,response_header.c_str(),response_header.size()) < 0){
+        std::cerr << "Sctp Send: " << std::strerror(errno) << std::endl;
+        munmap(mmap_file,file_size);
+        close(client_fd);
+        close(fd);
+        return false;
+    }
+
+    stream_index = (stream_index + 1) % MAX_STREAM_NUM;
+
+    // Send the file in chunks until it is processed
+    size_t current_sent = 0;
+    while(current_sent < file_size){
+
+        size_t bytes_to_send = std::min(CHUNK_SIZE,file_size-current_sent);
+
+        size_t bytes_sent = write(client_fd,
+                                         mmap_file + current_sent,
+                                         bytes_to_send);
+        if(bytes_sent < 0){
+            std::cerr << "Send: " << std::strerror(errno) << std::endl;
+            munmap(mmap_file,file_size);
+            close(client_fd);
+            close(fd);
+            return false;
+        }
+
+        stream_index = (stream_index + 1) % MAX_STREAM_NUM;
+        current_sent += CHUNK_SIZE;
+
+    }
+
+
+    close(fd);
+    munmap(mmap_file, file_size);
+    return true;
+
+}
+
+
+int main(){
+
+    // Change the working dir to the server root
+    if (chdir(SERVER_ROOT) != 0) {
+        std::cerr << "Chdir: " << strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Create the sctp socket
+    int sock_fd = socket(AF_INET,SOCK_STREAM,IPPROTO_SCTP);
+    if(sock_fd < 0){
+        std::cerr << "Socket: " << std::strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    sockaddr_in server_addr = {0};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // Reuse the address when needed
+    int opt = 1;
+    if(setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0){
+        std::cerr << "setsockopt: " << std::strerror(errno) << std::endl;
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+
+
+    // Enable data io
+    struct sctp_event_subscribe events;
+    bzero(&events,sizeof(events));
+    events.sctp_data_io_event = 1;
+
+    if(setsockopt(sock_fd,IPPROTO_SCTP,SCTP_EVENTS,&events,sizeof(events)) < 0){
+        perror("setsockopt");
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind the server to its address
+    if(bind(sock_fd, (sockaddr* ) &server_addr,sizeof(server_addr)) < 0){
+        std::cerr << "Bind: " << std::strerror(errno) << std::endl;
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Set the socket to listening mode
+    if(listen(sock_fd,1)  < 0){
+        std::cerr << "Listen: " << std::strerror(errno) << std::endl;
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    while(true){
+
+        sockaddr_in client_addr = {0};
+        socklen_t client_addr_size = sizeof(client_addr);
+
+        // Accept a new client
+        int client_fd = accept(sock_fd,(sockaddr*) &client_addr,&client_addr_size);
+
+        if(client_fd < 0){
+            std::cerr << "Accept: " << std::strerror(errno) << std::endl;
+            continue;
+        }
+
+        // Set the client receive buffer to provided size
+        if(setsockopt(client_fd, SOL_SOCKET, SO_SNDBUF, &SENDER_BUFFER_SIZE, sizeof(SENDER_BUFFER_SIZE)) < 0){
+            std::cerr << "setsockopt client: " << std::strerror(errno) << std::endl;
+            close(client_fd);
+            continue;
+        }
+
+        std::cout<<"New connection"<< std::endl;
+
+        int actual_size = 0;
+        unsigned int len = sizeof(actual_size);
+
+        // Get the actual size of the kernel receive buffer
+        if(getsockopt(client_fd, SOL_SOCKET, SO_SNDBUF, &actual_size, &len) < 0){
+            std::cerr << "getsockopt client: " << std::strerror(errno) << std::endl;
+            close(client_fd);
+            continue;
+        }
+
+        std::cout << "Size of kernel send buffer: "<< actual_size << std::endl;
+
+        while(true){
+            auto request = recv_response_header(client_fd);
+
+            if(request.empty())
+                break;
+
+            auto path = extract_http_path(request);
+
+            if(!send_file(client_fd,path))
+                break;
+
+        }
+
+        close(client_fd);
+
+    }
+
+    close(sock_fd);
+    return 0;
+}
